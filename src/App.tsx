ef786fa56f2c37ceab4678637263354ff32e27d7
Satish Dabhi <satish.d@engineersmind.com>
2026-05-26 14:03:22 +0530

    Added demo vite app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif',
+      fontSize: '2rem',
+      fontWeight: 'bold',
+      color: '#1a1a2e',
+    }}>
+      Hello from Satish
+    </div>
+  )
+}
+
+export default App
